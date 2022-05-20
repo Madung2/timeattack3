da0f791c8b3870acc8d19d44d12671a9df10a997
Madung2 <104334219+Madung2@users.noreply.github.com>
2022-05-20 20:02:15 +0900

    attack

---
+++ app.py
@@ -0,0 +1,26 @@
+import jwt
+import datetime
+import random
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.campProject
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
